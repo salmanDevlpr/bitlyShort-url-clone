cfccc8973506900932312837b3f3e5feb283a670
blacksalman <salman51180189@gmail.com>
2024-01-17 12:03:37 +0530

    added cors package

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose')
 require('dotenv').config();
+const cors = require('cors');
 
 const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 const PORT = process.env.PORT || 3000;
 
 main().catch(err => console.log(err));
